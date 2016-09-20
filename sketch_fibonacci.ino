c5e1ee7c1cc7169c46b5347d4517edbf1f5d02d5
Konstantin <kostozyb@gmail.com>
2016-09-20 04:29:39 +0400

    add init leds

--- sketch_fibonacci.ino
+++ sketch_fibonacci.ino
@@ -1,13 +1,32 @@
+#include <FastLED.h>
 #include <Wire.h>
 #include <TimeLib.h>
 #include <DS1307RTC.h>
 
+#define NUM_LEDS 9
+#define DATA_PIN 2
+
+CRGB leds[NUM_LEDS];
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) ; // wait for serial
+
+  FastLED.addLeds<WS2811, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
+  leds[0] = CRGB(255,0,0);
+  leds[1] = CRGB(0,255,0);
+  leds[2] = CRGB(0,0,255);
+  leds[3] = CRGB(255,255,0);
+  leds[4] = CRGB(255,255,0);
+  leds[5] = CRGB(0,255,255);
+  leds[6] = CRGB(0,255,255);
+  leds[7] = CRGB(0,255,255);
+  leds[8] = CRGB(0,255,255);
+  FastLED.show();//Посылаем данные на гирлянду
+  
   tmElements_t tm = getTime();
   time2console(tm);
   delay(1000);
