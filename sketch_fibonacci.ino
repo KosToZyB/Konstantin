7df46cdc8ec27cb0c02217ef4292316330e28ab1
Konstantin <kostozyb@gmail.com>
2016-11-01 21:59:27 +0400

    add mode light on/off

--- sketch_fibonacci.ino
+++ sketch_fibonacci.ino
@@ -17,7 +17,7 @@ enum SEQUENCE_ELEMENT {
   SE_SECOND = 1,
   SE_THREE = 2,
   SE_FOUR = 3,
-  SE_FIVE = 4  
+  SE_FIVE = 4
 };
 
 enum BUTTON_MODE {
@@ -27,11 +27,12 @@ enum BUTTON_MODE {
   BM_SCHEME = 3
 };
 
-SEQUENCE_ELEMENT typeLeds[NUM_LEDS] {SE_FIRST, 
-                                     SE_SECOND, 
-                                     SE_THREE, 
-                                     SE_FOUR, SE_FOUR, 
-                                     SE_FIVE, SE_FIVE, SE_FIVE, SE_FIVE};
+SEQUENCE_ELEMENT typeLeds[NUM_LEDS] {SE_FIRST,
+                   SE_SECOND,
+                   SE_THREE,
+                   SE_FOUR, SE_FOUR,
+                   SE_FIVE, SE_FIVE, SE_FIVE, SE_FIVE
+};
 
 enum CONTAINCE_VALUE {
   CV_NONE = 0,
@@ -42,17 +43,20 @@ enum CONTAINCE_VALUE {
 
 BUTTON_MODE gMode;
 
+bool gLightLed;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) ; // wait for serial
 
   gMode = BM_LIGHT;
-  
+  gLightLed = true;
+
   tmElements_t tm;
   if (!RTC.read(tm)) {
     if (RTC.chipPresent()) {
       Serial.println("need the SetTime");
-      
+
       getTime(__TIME__, tm);
       if (RTC.write(tm)) {
         Serial.println("set compile time");
@@ -70,27 +74,25 @@ void setup() {
 }
 
 void loop() {
-  FastLED.show();//Посылаем данные на гирлянду
+  FastLED.show(); 
   tmElements_t tm = getTime();
   time2console(tm);
   timeToLeds(tm.Hour, tm.Minute);
 
-  int buttonState1 = digitalRead(BTN_MODE);
-  if (buttonState1 == HIGH) { 
+  int btnMode = digitalRead(BTN_MODE);
+  if (btnMode == HIGH) {
     modeUp();
+    delay(500);
   }
 
   ledMode();
 
-  int buttonState2 = digitalRead(BTN_MODE);
-  if (buttonState2 == HIGH) { 
-    Serial.println("button on");
+  int btnSet = digitalRead(BTN_SET);
+  if (btnSet == HIGH) {
+	  setUp(gMode);
     delay(500);
   }
-  else {
-    Serial.println("button off");
-  }
-  
+
   delay(100);
 }
 
@@ -115,7 +117,7 @@ tmElements_t getTime() {
 
 void modeUp()
 {
-  switch(gMode) {
+  switch (gMode) {
     case BM_LIGHT:
       gMode = BM_HOUR;
       break;
@@ -123,23 +125,23 @@ void modeUp()
       gMode = BM_MINUTE;
       break;
     case BM_MINUTE:
-      gMode = BM_SCHEME;    
+      gMode = BM_SCHEME;
       break;
     case BM_SCHEME:
-      gMode = BM_LIGHT;       
-      break;    
+      gMode = BM_LIGHT;
+      break;
     default:
       gMode = BM_LIGHT;
   }
 }
 
 void time2console(const tmElements_t &tm) {
-//  print2digits(tm.Hour);
-//  Serial.write(':');
-//  print2digits(tm.Minute);
-//  Serial.write(':');
-//  print2digits(tm.Second);
-//  Serial.println();
+  //  print2digits(tm.Hour);
+  //  Serial.write(':');
+  //  print2digits(tm.Minute);
+  //  Serial.write(':');
+  //  print2digits(tm.Second);
+  //  Serial.println();
 }
 
 void print2digits(const int number) {
@@ -150,125 +152,127 @@ void print2digits(const int number) {
 }
 
 void timeToLeds(const uint8_t hour, const uint8_t minute) {
-  CONTAINCE_VALUE leds[5] = {CV_NONE, CV_NONE, CV_NONE, CV_NONE, CV_NONE}; 
-  hourToLeds(hour, leds);
-  minuteToLeds(minute, leds);
+  CONTAINCE_VALUE leds[5] = {CV_NONE, CV_NONE, CV_NONE, CV_NONE, CV_NONE};
+  if (gLightLed) {
+    hourToLeds(hour, leds);
+    minuteToLeds(minute, leds);
+  }
   setColorLed(leds);
 }
 
 void hourToLeds(uint8_t hour, CONTAINCE_VALUE leds[]) {
   switch (hour % 12) {
-  case 1: // 1, 13
-    setHourToElement(leds[0]);
-    break;
-  case 2: // 2, 14
-    setHourToElement(leds[2]);
-    break;
-  case 3: // 3, 15
-    setHourToElement(leds[3]);
-    break;
-  case 4: // 4, 16
-    setHourToElement(leds[0]);
-    setHourToElement(leds[3]);
-    break;
-  case 5: // 5, 17
-    setHourToElement(leds[4]);
-    break;
-  case 6: // 6, 18
-    setHourToElement(leds[0]);
-    setHourToElement(leds[4]);
-    break;
-  case 7: // 7, 19
-    setHourToElement(leds[2]);
-    setHourToElement(leds[4]);
-    break;
-  case 8: // 8, 20
-    setHourToElement(leds[3]);
-    setHourToElement(leds[4]);
-    break;
-  case 9: // 9, 21
-    setHourToElement(leds[0]);
-    setHourToElement(leds[3]);
-    setHourToElement(leds[4]);
-    break;
-  case 10: // 10, 22
-    setHourToElement(leds[2]);
-    setHourToElement(leds[3]);
-    setHourToElement(leds[4]);
-    break;
-  case 11: // 11, 23
-    setHourToElement(leds[0]);
-    setHourToElement(leds[2]);
-    setHourToElement(leds[3]);
-    setHourToElement(leds[4]);
-    break;
-  case 0: // 12, 0
-    setHourToElement(leds[0]);
-    setHourToElement(leds[1]);
-    setHourToElement(leds[2]);
-    setHourToElement(leds[3]);
-    setHourToElement(leds[4]);
-    break;
-  default:
-    break;
+    case 1: // 1, 13
+      setHourToElement(leds[0]);
+      break;
+    case 2: // 2, 14
+      setHourToElement(leds[2]);
+      break;
+    case 3: // 3, 15
+      setHourToElement(leds[3]);
+      break;
+    case 4: // 4, 16
+      setHourToElement(leds[0]);
+      setHourToElement(leds[3]);
+      break;
+    case 5: // 5, 17
+      setHourToElement(leds[4]);
+      break;
+    case 6: // 6, 18
+      setHourToElement(leds[0]);
+      setHourToElement(leds[4]);
+      break;
+    case 7: // 7, 19
+      setHourToElement(leds[2]);
+      setHourToElement(leds[4]);
+      break;
+    case 8: // 8, 20
+      setHourToElement(leds[3]);
+      setHourToElement(leds[4]);
+      break;
+    case 9: // 9, 21
+      setHourToElement(leds[0]);
+      setHourToElement(leds[3]);
+      setHourToElement(leds[4]);
+      break;
+    case 10: // 10, 22
+      setHourToElement(leds[2]);
+      setHourToElement(leds[3]);
+      setHourToElement(leds[4]);
+      break;
+    case 11: // 11, 23
+      setHourToElement(leds[0]);
+      setHourToElement(leds[2]);
+      setHourToElement(leds[3]);
+      setHourToElement(leds[4]);
+      break;
+    case 0: // 12, 0
+      setHourToElement(leds[0]);
+      setHourToElement(leds[1]);
+      setHourToElement(leds[2]);
+      setHourToElement(leds[3]);
+      setHourToElement(leds[4]);
+      break;
+    default:
+      break;
   }
 }
 
 void minuteToLeds(uint8_t minute, CONTAINCE_VALUE leds[]) {
   switch (minute / 5) {
-  case 1: // 1
-    setMinuteToElement(leds[0]);
-    break;
-  case 2: // 2
-    setMinuteToElement(leds[2]);
-    break;
-  case 3: // 3
-    setMinuteToElement(leds[3]);
-    break;
-  case 4: // 4
-    setMinuteToElement(leds[0]);
-    setMinuteToElement(leds[3]);
-    break;
-  case 5: // 5
-    setMinuteToElement(leds[4]);
-    break;
-  case 6: // 6
-    setMinuteToElement(leds[0]);
-    setMinuteToElement(leds[4]);
-    break;
-  case 7: // 7
-    setMinuteToElement(leds[2]);
-    setMinuteToElement(leds[4]);
-    break;
-  case 8: // 8
-    setMinuteToElement(leds[3]);
-    setMinuteToElement(leds[4]);
-    break;
-  case 9: // 9
-    setMinuteToElement(leds[0]);
-    setMinuteToElement(leds[3]);
-    setMinuteToElement(leds[4]);
-    break;
-  case 10: // 10
-    setMinuteToElement(leds[2]);
-    setMinuteToElement(leds[3]);
-    setMinuteToElement(leds[4]);
-    break;
-  case 11: // 11
-    setMinuteToElement(leds[0]);
-    setMinuteToElement(leds[2]);
-    setMinuteToElement(leds[3]);
-    setMinuteToElement(leds[4]);
-    break;
-  case 0: // 12
-    setMinuteToElement(leds[0]);
-    setMinuteToElement(leds[1]);
-    setMinuteToElement(leds[2]);
-    setMinuteToElement(leds[3]);
-    setMinuteToElement(leds[4]);
-    break;
-  default:
-    break;
+    case 1: // 1
+      setMinuteToElement(leds[0]);
+      break;
+    case 2: // 2
+      setMinuteToElement(leds[2]);
+      break;
+    case 3: // 3
+      setMinuteToElement(leds[3]);
+      break;
+    case 4: // 4
+      setMinuteToElement(leds[0]);
+      setMinuteToElement(leds[3]);
+      break;
+    case 5: // 5
+      setMinuteToElement(leds[4]);
+      break;
+    case 6: // 6
+      setMinuteToElement(leds[0]);
+      setMinuteToElement(leds[4]);
+      break;
+    case 7: // 7
+      setMinuteToElement(leds[2]);
+      setMinuteToElement(leds[4]);
+      break;
+    case 8: // 8
+      setMinuteToElement(leds[3]);
+      setMinuteToElement(leds[4]);
+      break;
+    case 9: // 9
+      setMinuteToElement(leds[0]);
+      setMinuteToElement(leds[3]);
+      setMinuteToElement(leds[4]);
+      break;
+    case 10: // 10
+      setMinuteToElement(leds[2]);
+      setMinuteToElement(leds[3]);
+      setMinuteToElement(leds[4]);
+      break;
+    case 11: // 11
+      setMinuteToElement(leds[0]);
+      setMinuteToElement(leds[2]);
+      setMinuteToElement(leds[3]);
+      setMinuteToElement(leds[4]);
+      break;
+    case 0: // 12
+      setMinuteToElement(leds[0]);
+      setMinuteToElement(leds[1]);
+      setMinuteToElement(leds[2]);
+      setMinuteToElement(leds[3]);
+      setMinuteToElement(leds[4]);
+      break;
+    default:
+      break;
   }
 }
 
@@ -289,29 +293,28 @@ void setMinuteToElement(CONTAINCE_VALUE &led) {
 }
 
 void setColorLed(CONTAINCE_VALUE leds[]) {
-   for (char j = 0; j < NUM_LEDS; j++) {
+  for (char j = 0; j < NUM_LEDS; j++) {
     SEQUENCE_ELEMENT type = typeLeds[j];
     switch (leds[type]) {
-    case CV_NONE:
-      colorLeds[j] = CRGB(0,0,0);
-      break;
-    case CV_MINUTE:
-      colorLeds[j] = CRGB(255,0,0);
-      break;
-   case CV_HOUR:
-      colorLeds[j] = CRGB(0,255,0);
-      break;
-    case CV_HOUR_WITH_MINUTE:
-      colorLeds[j] = CRGB(0,0,255);
-      break;
-    default:
-      colorLeds[j] = CRGB(0,0,0);
+      case CV_NONE:
+        colorLeds[j] = CRGB(0, 0, 0);
+        break;
+      case CV_MINUTE:
+        colorLeds[j] = CRGB(255, 0, 0);
+        break;
+      case CV_HOUR:
+        colorLeds[j] = CRGB(0, 255, 0);
+        break;
+      case CV_HOUR_WITH_MINUTE:
+        colorLeds[j] = CRGB(0, 0, 255);
+        break;
+      default:
+        colorLeds[j] = CRGB(0, 0, 0);
     }
-   }
+  }
 }
 
-bool getTime(const char *str, tmElements_t &tm)
-{
+bool getTime(const char *str, tmElements_t &tm) {
   int Hour, Min, Sec;
 
   if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
@@ -321,9 +324,8 @@ bool getTime(const char *str, tmElements_t &tm)
   return true;
 }
 
-void ledMode()
-{
-  switch(gMode) {
+void ledMode() {
+  switch (gMode) {
     case BM_LIGHT:
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
@@ -334,15 +336,49 @@ void ledMode()
       break;
     case BM_MINUTE:
       digitalWrite(LED1, LOW);
-      digitalWrite(LED2, HIGH);    
+      digitalWrite(LED2, HIGH);
       break;
     case BM_SCHEME:
       digitalWrite(LED1, HIGH);
-      digitalWrite(LED2, HIGH);     
-      break;    
+      digitalWrite(LED2, HIGH);
+      break;
     default:
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
   }
 }
 
+void setUp(const BUTTON_MODE &mode) {
+  switch (mode) {
+    case BM_LIGHT:
+      lightUp();
+      break;
+    case BM_HOUR:
+      hourUp();
+      break;
+    case BM_MINUTE:
+      minuteUp();
+      break;
+    case BM_SCHEME:
+      schemeUp();
+      break;
+    default:
+      schemeUp();
+  }
+}
+
+void lightUp() {
+	gLightLed = !gLightLed;
+}
+
+void hourUp() {
+	
+}
+
+void minuteUp() {
+	
+}
+
+void schemeUp() {
+	
+}
